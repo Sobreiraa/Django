ebae44f1a4e8be275274e12cd80e7171b7c47ec6
Matheus Sobreira <matheussobreira2702@gmail.com>
2023-01-05 11:13:05 -0300

    formulário django

--- Django-Luiz-Otavio-Miranda/django-projeto1/authors/forms.py
+++ Django-Luiz-Otavio-Miranda/django-projeto1/authors/forms.py
@@ -10,4 +10,32 @@ class RegisterForm(forms.ModelForm):
             'username',
             'email',
             'password',
-        ]
+        ] # Escolhendo os campos que desejamos
+
+        #exclude = ['first_name']  Excluindo os campos que desejamos
+        labels = {
+            'first_name': 'First Name:',
+            'last_name': 'Last name:',
+            'username': 'Username:',
+            'email': 'E-mail:',
+            'password': 'Password:'
+        }
+        
+        help_texts = {
+            'email': 'The e-mail must be valid.'
+        }
+
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty.',
+            }
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your first name here.'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here.'
+            })
+        }
